e84fa36b652a9ac41a7cbf0be6cafb2be6c2e023
Jon Hannah <jon@synistree.com>
2021-01-24 16:01:35 -0800

    Establishing devel branch

--- examples/reminder.py
+++ examples/reminder.py
@@ -23,8 +23,7 @@ class Reminder(RedisEntry):
     @property
     def trigger_dt(self) -> datetime:
         """
-        Property wrapper for converting the float ``trigger_ts`` into
-        a ``datetime``
+        Property wrapper for converting the float "trigger_ts" into "datetime"
         """
 
         return datetime.fromtimestamp(self.trigger_ts)
@@ -32,16 +31,15 @@ class Reminder(RedisEntry):
     @property
     def created_dt(self) -> datetime:
         """
-        Property wrapper for converting the float ``created_ts`` into
-        a ``datetime``
+        Property wrapper for converting the float "created_ts" into "datetime"
         """
 
         return datetime.fromtimestamp(self.created_ts)
 
     def __post_init__(self) -> None:
         """
-        Set the default ``is_complete`` field based on current timestamp and
-        sets ``redis_name`` based on member and trigger attributes
+        Set the default "is_complete" field based on current timestamp and
+        sets "redis_name" based on member and trigger attributes
         """
 
         self.redis_name = f'{self.member_id}:{self.trigger_ts}'
